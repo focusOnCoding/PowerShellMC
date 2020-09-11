c0e6449fe3be1e87a4ffd4027ec9e9991a4e230f
Siyabonga <focusoncoding@gmail.com>
2020-09-11 05:41:47 +0200

    adding a new file

---
+++ git and github/github.ps1
@@ -0,0 +1,13 @@
+# get the version
+git --version
+
+# create the user 
+git config --global user.name UserNameGoesHere
+
+# user email
+git config --global user.email EmailGoesHere
+
+# change dir
+cd .. 
+
+touch index.html
